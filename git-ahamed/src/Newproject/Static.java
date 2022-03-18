9109fb62fa02e53d09fa596331537bbc39360a5a
ameed <ameed@DESKTOP-NJ1P7TC>
2022-03-18 20:44:16 +0530

    Static Method

---
+++ git-ahamed/src/Newproject/Static.java
@@ -0,0 +1,19 @@
+package Newproject;
+
+public class Static {
+	public static void main(String[] args) {
+		Static.met();
+		System.out.println(Static.met2());
+	}
+ static void met() {
+	 System.out.println("Static met called....");
+ }
+ static int met2() {
+	 System.out.println("Met2 called....");
+	 return 20;
+ }
+}
+
+
+	
+ 
